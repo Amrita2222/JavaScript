bf6e09d4f1b8cf9ecfb62b69591264dd21bf33b2
Rita Roy <122859805+Amrita2222@users.noreply.github.com>
2023-08-07 14:44:20 +0000

    Pending changes exported from your codespace

---
+++ 01_Basics/03_datatypeconversion.js
@@ -0,0 +1,32 @@
+// let number = "55";
+// let number = null;
+// let number = undefine;
+// let number = true;
+let number = false;
+
+// console.log(typeof number);
+
+let valueInInteger = Number (number);
+// console.log(typeof valueInInteger);
+// console.log(valueInInteger);
+
+
+// Note : 
+// "33" => 33
+// "33abc => NaN
+// true => 1;  false => 0
+
+
+// Conversion of Boolean
+
+let Booleanvalue = 1
+let Booleanlogged = Boolean(Booleanvalue);
+console.log(Booleanlogged);
+
+let falsevalue = 0;
+let booleanfalsevalue = Boolean(falsevalue);
+console.log(booleanfalsevalue);
+
+let somenumber = 33;
+let stringnumber = String(somenumber);
+console.log(typeof stringnumber);
---
+++ 01_Basics/04_stringtonumber.js
@@ -0,0 +1,7 @@
+// Operators
+
+let value = 55;
+let negvalue = -value;
+console.log(2 +2);
+console.log('1' + 2 + 2);
+console.log()
