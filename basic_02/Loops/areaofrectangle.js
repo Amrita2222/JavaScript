62fa4785e23c23f13abb519e2447a7728c46a72f
Rita <amrita.spj2222@gmail.com>
2024-01-18 21:23:19 +0530

    new file add

--- basic_02/Loops/areaofrectangle.js
+++ basic_02/Loops/areaofrectangle.js
@@ -1,18 +1,18 @@
-// Get user input for length
-var length = parseFloat("Enter the length of the rectangle:");
+// // Get user input for length
+// var length = parseFloat("Enter the length of the rectangle:");
 
-// Get user input for width
-var width = parseFloat("Enter the width of the rectangle:");
+// // Get user input for width
+// var width = parseFloat("Enter the width of the rectangle:");
 
-// Check if the inputs are valid numbers
-if (isNaN(length) || isNaN(width) || length <= 0 || width <= 0) {
-    console.log();("Invalid input. Please enter valid positive numbers for length and width.");
-} else {
-    // Calculate the area
-    var area = length * width;
+// // Check if the inputs are valid numbers
+// if (isNaN(length) || isNaN(width) || length <= 0 || width <= 0) {
+//     console.log();("Invalid input. Please enter valid positive numbers for length and width.");
+// } else {
+//     // Calculate the area
+//     var area = length * width;
 
-    // Display the result
+//     // Display the result
 
-console.log(area);
-}
+// console.log(area);
+// }
 
